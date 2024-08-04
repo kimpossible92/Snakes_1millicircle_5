684a0e6c59503cb5acfed89cdb4e1fa3364711fd
kimpossible92 <kim.possible.92@bk.ru>
2024-08-05 05:33:09 +0900

    sdasdasd
    asdsadsad

--- Snakes_1millicircle/Assets/Game/Scripts/Core/ICollectable.cs
+++
@@ -1,7 +0,0 @@
-namespace RPG.Core
-{
-    public interface ICollectable
-    {
-        void Collect();
-    }
-}
--- Snakes_1millicircle/Assets/Scripts/Character/Combat/TargetableScript.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TargetableScript : MonoBehaviour
-{
-    public enum EnemyType { Minion, EnemyHero, AllyHero, Hero, FriendlyMinion,any }
-    public EnemyType enemyType;
-}
